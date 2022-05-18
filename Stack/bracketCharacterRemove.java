0dd4072376ac981041bb99ab480564026b62d3c3
hanseu9839 <hanseu9839@gmail.com>
2022-05-18 16:47:51 +0900

    bracketCharacterRemove

--- Stack/bracketCharacterRemove.java
+++ Stack/bracketCharacterRemove.java
@@ -7,6 +7,15 @@ public class bracketCharacterRemove {
     public static String solution(String str){
         String answer= "";
         Stack<Character> stack = new Stack<>();
+        for (char x : str.toCharArray()){
+            if(x==')'){
+                while(stack.pop()!='('); //pop은 꺼낸 것을 리턴을 받는다. 
+            }else stack.push(x);
+        }
+        for(int i=0;i<stack.size();i++){
+            answer+=stack.get(i);
+        }
+        
         // for(char s : str.toCharArray()){
         //     if(s=='(')
         //         stack.push(s);
@@ -16,7 +25,7 @@ public class bracketCharacterRemove {
         //         if(stack.isEmpty()) answer+=s;
         //     }
         // }
-        
+
         return answer; 
     }
     public static void main(String[] args) {
