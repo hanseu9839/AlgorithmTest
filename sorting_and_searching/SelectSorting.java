d13d363fd15037b4b601e0e8e6110e741bdd5cdd
hanseu9839 <hanseu9839@gmail.com>
2022-05-20 14:08:02 +0900

    SelectSorting

---
+++ sorting_and_searching/SelectSorting.java
@@ -0,0 +1,37 @@
+package sorting_and_searching;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SelectSorting {
+    public static ArrayList<Integer> solution(int[] arr,int num){
+        ArrayList<Integer> answer = new ArrayList<>();
+        int idx = 0;
+        for(int i=0;i<num;i++){
+            idx=i;
+            for(int j=i+1;j<num;j++){
+                if(arr[j]<arr[idx]) idx=j;
+            }
+            int tmp = arr[idx];
+            arr[idx] = arr[i];
+            arr[i] = tmp;
+        }
+        for(int i=0;i<num;i++){
+            answer.add(arr[i]);
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+
+        int []arr= new int[num];
+        for(int i=0;i<num;i++){
+            arr[i]=scan.nextInt();
+        }
+        for(int x : solution(arr,num)){
+            System.out.print(x+" ");
+        }
+    }
+}
