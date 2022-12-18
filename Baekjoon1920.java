a860e34db9c0c08edea8fac67d704c94de3410f4
hanseu9839 <hanseu9839@gmail.com>
2022-12-18 15:33:26 +0900

    22_12_18(Study)_BinarySearch_Resovle

--- Baekjoon1920.java
+++ Baekjoon1920.java
@@ -1,5 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Baekjoon1920 {
-    public static void main(String[] args) {
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
+        int N = Integer.parseInt(br.readLine());
+        int []arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int rnt = 0;
+        while(st.hasMoreTokens()){
+            arr[rnt] = Integer.parseInt(st.nextToken());
+            rnt++;
+        }
+        Arrays.sort(arr);
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        
+        for(int i=0;i<M;i++){
+            if(binarySearch(arr, Integer.parseInt(st.nextToken())) >= 0){
+                sb.append(1).append('\n');
+            }
+            else{
+                sb.append(0).append('\n');
+            }
+        }
+        System.out.println(sb);
+        
+    }
+    static int binarySearch(int[] arr, int num){
+        int lo =0;
+        int hi = arr.length-1;
+        
+        while(lo<=hi){
+            int mid = (lo + hi) /2;
+            if(arr[mid]<num){
+                lo = mid + 1;
+            }else if(arr[mid]>num){
+                hi = mid - 1;
+            }else{
+                return mid;
+            } 
+        }
+        return -1;
     }
 }
